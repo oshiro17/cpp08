003bb83eeb3ebcfa7cc9c5d4e79e5c1d0323abe1
Nonoka <nonokapiano@icloud.com>
2024-11-14 00:56:42 +0900

    add test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -21,5 +21,30 @@ int main(void)
     catch (const std::exception& e) {
         std::cout << "Error: " << e.what() << std::endl;
     }
+    	std::cout << "\nNG case " << std::endl;
+	{	
+		Span sp(5);
+
+		try {
+			std::cout << sp.shortestSpan() << std::endl;
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << sp.longestSpan() << std::endl;
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			sp.addNumber(1);
+			sp.addNumber(2);
+			sp.addNumber(3);
+			sp.addNumber(4);
+			sp.addNumber(5);
+			sp.addNumber(6);
+		} catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
 
 }
