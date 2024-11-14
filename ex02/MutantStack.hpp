b42776b212f66046e9ad1207e1e8693df0083f4b
Nonoka <nonokapiano@icloud.com>
2024-11-14 10:17:20 +0900

    クラス内で使うイテレーターの型エイリアスを定義

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -8,6 +8,12 @@
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
     public:
+
+        // typedef typename Container::iterator iterator;
+        typedef typename Container::const_iterator const_iterator;
+        typedef typename Container::reverse_iterator reverse_iterator;
+        typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
         MutantStack() : std::stack<T, Container>() {}
         MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
         ~MutantStack() {}
@@ -19,15 +25,16 @@ class MutantStack : public std::stack<T, Container> {
             return *this;
         }
 
-        typename Container::iterator begin() { return this->c.begin(); }
-        typename Container::iterator end() { return this->c.end(); }
-        typename Container::const_iterator begin() const { return this->c.begin(); }
-        typename Container::const_iterator end() const { return this->c.end(); }
+        // Container::iterator begin() { return this->c.begin(); }
+        typename iterator begin() { return this->c.begin(); }
+        const_iterator begin() const { return this->c.begin(); }
+        typeiterator end() { return this->c.end(); }
+        const_iterator end() const { return this->c.end(); }
 
-        typename Container::reverse_iterator rbegin() { return this->c.rbegin(); }
-        typename Container::reverse_iterator rend() { return this->c.rend(); }
-        typename Container::const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-        typename Container::const_reverse_iterator rend() const { return this->c.rend(); }
+        reverse_iterator rbegin() { return this->c.rbegin(); }
+        const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+        reverse_iterator rend() { return this->c.rend(); }
+        const_reverse_iterator rend() const { return this->c.rend(); }
 };
 
 #endif
